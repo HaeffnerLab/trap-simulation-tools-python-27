cc10f44a18c68e65556a722487ab298cd9f0324f
Tokumaru <tokumaru@berkeley.edu>
2014-05-01 22:36:03 -0700

    naming conventions for testing.py

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-from all_functions import import_data,get_trap,expand_field,trap_knobs,set_dc,ppt3
+from all_functions import import_data,get_trap,expand_field,trap_knobs,set_dc,post_process_trap
 from test_cube import test_cube
 
 #print(test_cube())
@@ -7,4 +7,4 @@ from test_cube import test_cube
 #print(expand_field())
 #print(trap_knobs())
 #print(set_dc()) 
-print (ppt3()) # test data has no saddle for multipoleControls=True for setdc
+print (post_process_trap()) # test data has no saddle for multipoleControls=True for setdc
